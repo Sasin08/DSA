a256af8e67429b3a521aa66785e9025e69974457
Sasin08 <148642997+Sasin08@users.noreply.github.com>
2025-07-07 13:21:54 +0530

    Create Solution.py

---
+++ Searching and Sorting/Find Peak Element/Solution.py
@@ -0,0 +1,12 @@
+def findPeakElement(self, nums: List[int]) -> int:
+    l = 0
+    r = len(nums)-1
+    while l <= r:
+        mid = (l+r)//2
+        if nums[mid] > nums[mid-1] and nums[mid] > nums[mid+1]:
+            return mid
+        elif nums[mid+1] > nums[mid]:
+            l = mid+1
+        else:
+            r = mid-1
+    return l
