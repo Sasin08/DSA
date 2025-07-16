7b0fee68761cdb5b352b384991fb329b33c024f7
Sasin08 <148642997+Sasin08@users.noreply.github.com>
2025-07-16 11:03:30 +0530

    Create Solution.py

---
+++ Array and String/Maximum Nesting Depth of the Parentheses/Solution.py
@@ -0,0 +1,10 @@
+def maxDepth(self, s: str) -> int:
+    count = 0
+    res = 0
+    for i in s:
+        if i == "(":
+            count += 1
+            res = max(res, count)
+        elif i == ")":
+            count -= 1
+    return res
